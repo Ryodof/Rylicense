40148a8a8527a6e749a6cbe6e9d9ac9a83960167
ryodof <ryodof.work@yandex.ru>
2025-05-28 20:32:55 +0300

    update

--- cli.py
+++ cli.py
@@ -1,12 +1,20 @@
-import colorama
 from rich import print
 from questionary import Style
 import questionary
+from generator import mit
+import time
 
 
 print("[bold cyan] License Generator[/bold cyan]" )
 
 options = ["MIT", "Apache", "Custom", "Exit"]
+
+commands = {
+    "exit":"exit()",
+    "MIT":"mit()",
+    "Apache":"apache()",
+    "Custom":"cst()",
+}
 
 dark_style = Style([
     ("question", "fg:#A0A0FF bold"),      
@@ -19,8 +27,7 @@ choice = questionary.select(
     "Выберите тип лицензии для вашего проекта: ",
     choices=options,
     qmark="-->", 
-    style= dark_style,
-    
+    style= dark_style, 
 ).ask()
 
 if choice == "Exit":
@@ -28,4 +35,8 @@ if choice == "Exit":
     exit()
 else:
     if choice != "Exit":
-        print(f"Вы выбрали: {choice}")
+        print(f"[green] Вы выбрали: {choice}[/green]")
+    if choice == "MIT":
+        name = input("Type your name:  ")
+        year = str(time.localtime().tm_year)
+        mit(name, year)
---
+++ generator.py
@@ -0,0 +1,17 @@
+import os
+from jinja2 import Template
+
+def mit(name: str, year: str):
+    os.makedirs("output", exist_ok=True)
+    
+    if not name or not name.strip():
+        name = "Anonymous"
+    
+    with open("templates/mit.txt", "r") as f:
+        mit_templ = f.read()
+        template = Template(mit_templ)
+        license_text = template.render(author=name, year=year)
+    
+    with open("output/output.txt", "w") as q:
+        q.write(license_text)
+    print(license_text)
