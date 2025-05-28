a8de960c71592f5a442b1221b927cbffd74d043e
ryodof <ryodof.work@yandex.ru>
2025-05-28 23:42:59 +0300

    hellll

--- generator.py
+++ generator.py
@@ -17,4 +17,18 @@ def mit(name: str, year: str):
     print(license_text)
     
 #def apache():
+
+def bsd(name:str, year:str, ):
+    os.makedirs("output", exist_ok=True)
     
+    if not name or not name.strip():
+        name = "Anonymous"
+    
+    with open("templates/bsd.txt", "r") as f:
+        mit_templ = f.read()
+        template = Template(mit_templ)
+        license_text = template.render(author=name, year=year)
+    
+    with open("output/output.txt", "w") as q:
+        q.write(license_text)
+    print(license_text)
